36347dfaf305f2da3cf9a960d0985d94fe4cea9f
Carol <carolbrandes23@gmail.com>
2020-03-14 22:08:52 -0300

    fiz o menu

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <title>RM Veículos Especiais</title>
+        <meta name="description" content="Site para consultoria, compra e venda de automóveis">
+        <meta name="keywords" content="carros, automóveis, veículos">
+        <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+        <contexto>
+                <header>
+                        <img class="logo" src="imagens/logo.jpg" alt="logo RM Veículos Especiais">
+                        <nav class="menu-mobile">
+                                <img src="imagens/menu-button.png" alt="menu" title="menu">
+                        </nav>
+                        <nav class="menu">
+                                <a href="index.php">Home</a>
+                                <a href="">Venda</a>
+                                <a href="">Galeria</a>
+                                <a href="">Eventos</a>
+                                <a href="">Sobre Nós</a>
+                                <a href="">Contato</a>
+                        </nav>
+                </header>
+        </contexto>
+
+
+        <script src="js/menu.js"></script>
+
+</body>
+
+</html>
