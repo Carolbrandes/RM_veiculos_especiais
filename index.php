9ec93d20793f439fc214ded477799ef16c1f05d5
Carol <carolbrandes23@gmail.com>
2020-03-19 14:41:44 -0300

    fiz a secao contato e ajustes em todas as outras secoes nas medias queries

--- index.php
+++ index.php
@@ -120,6 +120,37 @@
         </section>
 
 
+        <section class="form-contato">
+                <contexto>
+                        <div class="titulo-linha">
+                                <div class="titulo-linha__linha"></div>
+                                <h1 class="titulo-linha__titulo">Entre em Contato</h1>
+                        </div>
+
+                        <form class="form-contato__form" action="" method="post">
+                                <div class="w-100">
+                                        <input type="text" name="nome" placeholder="Nome" required>
+                                </div>
+
+                                <div class="w-50">
+                                        <div class="w-50__elemento">
+                                                <input  type="email" name="email" placeholder="E-mail" required>
+                                        </div>
+                                        <div class="w-50__elemento">
+                                                <input type="text" name="telefone" placeholder="Telefone" required>
+                                        </div>
+                                </div>
+
+                                <div class="w-100">
+                                        <textarea name="mensagem" placeholder="Mensagem" required></textarea>
+                                </div>
+
+                                <button type="submit" name="acao" class="botao-mais-detalhes">Enviar</button>
+                        </form>
+                </contexto>
+        </section>
+
+
         <script src="js/menu.js"></script>
 
 </body>
