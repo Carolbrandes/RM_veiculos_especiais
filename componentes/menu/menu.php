cdfe3e7eb0b8563d745f8b64b5c8d9e22cd9c921
Carol <carolbrandes23@gmail.com>
2020-03-24 20:45:30 -0300

    fiz a pagina sobre nos

--- componentes/menu/menu.php
+++ componentes/menu/menu.php
@@ -1,6 +1,4 @@
 <a href="index.php">Home</a>
 <a href="venda.php">Venda</a>
-<a href="">Galeria</a>
-<a href="">Eventos</a>
-<a href="">Sobre Nós</a>
+<a href="sobre-nos.php">Sobre Nós</a>
 <a href="">Contato</a>
---
+++ sobre-nos.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <title>RM Veículos Especiais</title>
+        <meta name="description" content="Site para consultoria, compra e venda de automóveis">
+        <meta name="keywords" content="carros, automóveis, veículos">
+        <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+        <?php include_once "componentes/header/header.php"; ?>
+
+        <section class="sobre-nos">
+                <contexto>
+                        <div class="titulo-linha">
+                                <div class="titulo-linha__linha"></div>
+                                <h1 class="titulo-linha__titulo">Sobre Nós</h1>
+                        </div>
+
+                        <section class="sobre-nos__banner"></section>
+
+                        <h2 class="titulo-simples-preto">A RM Veículos Especiais</h2>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, optio fuga architecto possimus voluptatibus nihil quasi quas deserunt molestias iure eos amet corporis aut dolor illo, quis nobis tempore nesciunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae esse accusantium eaque cum officia, labore odit nobis alias, asperiores dignissimos adipisci. Amet similique at modi obcaecati voluptatem. Veritatis, voluptates dolores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iste assumenda voluptate laudantium laboriosam neque dignissimos repellat consectetur. Architecto unde impedit veniam nihil quia! Ut ex dignissimos cumque necessitatibus laborum.</p>
+
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, optio fuga architecto possimus voluptatibus nihil quasi quas deserunt molestias iure eos amet corporis aut dolor illo, quis nobis tempore nesciunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae esse accusantium eaque cum officia, labore odit nobis alias, asperiores dignissimos adipisci. Amet similique at modi obcaecati voluptatem. Veritatis, voluptates dolores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iste assumenda voluptate laudantium laboriosam neque dignissimos repellat consectetur. Architecto unde impedit veniam nihil quia! Ut ex dignissimos cumque necessitatibus laborum.</p>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, optio fuga architecto possimus voluptatibus nihil quasi quas deserunt molestias iure eos amet corporis aut dolor illo, quis nobis tempore nesciunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae esse accusantium eaque cum officia, labore odit nobis alias, asperiores dignissimos adipisci. Amet similique at modi obcaecati voluptatem. Veritatis, voluptates dolores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iste assumenda voluptate laudantium laboriosam neque dignissimos repellat consectetur. Architecto unde impedit veniam nihil quia! Ut ex dignissimos cumque necessitatibus laborum.</p>
+
+
+                        <section class="sobre-nos__fundador">
+                                <div class="sobre-nos__foto-fundador"></div>
+
+                                <div class="sobre-nos__texto-fundador">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nulla laudantium quia eos cumque! Recusandae eos eveniet fugiat numquam. Nostrum debitis accusantium hic numquam provident temporibus? Placeat quod dignissimos sint.</p>
+
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nulla laudantium quia eos cumque! Recusandae eos eveniet fugiat numquam. Nostrum debitis accusantium hic numquam provident temporibus? Placeat quod dignissimos sint.</p>
+
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nulla laudantium quia eos cumque! Recusandae eos eveniet fugiat numquam. Nostrum debitis accusantium hic numquam provident temporibus? Placeat quod dignissimos sint.</p>
+                                </div>
+                        </section>
+
+                </contexto>
+
+        </section>
+
+
+        <?php include_once "componentes/footer/footer.php"; ?>
+
+        <script src="js/menu.js"></script>
+
+</body>
+
+</html>
