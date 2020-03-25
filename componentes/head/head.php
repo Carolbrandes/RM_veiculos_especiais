e2c70fd9af267c4f32206b08365dbefc04defbe3
Carol <carolbrandes23@gmail.com>
2020-03-24 21:59:20 -0300

    Coloquei o icone no title

---
+++ componentes/head/head.php
@@ -0,0 +1,10 @@
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <title>RM Veículos Especiais</title>
+        <link rel="shortcut icon" type="image-x/png" href="imagens/icons8-carro-50.ico" />
+        <meta name="description" content="Site para consultoria, compra e venda de automóveis">
+        <meta name="keywords" content="carros, automóveis, veículos">
+        <link rel="stylesheet" href="css/style.css">
+</head>
--- veiculo.php
+++ veiculo.php
@@ -1,15 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-        <title>RM Veículos Especiais</title>
-        <meta name="description" content="Site para consultoria, compra e venda de automóveis">
-        <meta name="keywords" content="carros, automóveis, veículos">
-        <link rel="stylesheet" href="css/style.css">
-</head>
+<?php include_once "componentes/head/head.php"; ?>
 
 <body>
 
